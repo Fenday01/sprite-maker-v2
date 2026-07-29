8d5d64d2288408713c9e8af7614cdd33b2fdc463
Frankie Endley <frankie@made-pd.co.uk>
2026-07-29 11:14:12 +0100

    Keep Select active when resetting selection

--- main.py
+++ main.py
@@ -39,7 +39,6 @@ class PixelArtApp:
         self.height_var = tk.StringVar(value=str(self.grid_height))
         self.brush_size_var = tk.IntVar(value=1)
         self.brush_shape_var = tk.StringVar(value="Square")
-        self.last_paint_tool = "Square"
         self.sprite_name_var = tk.StringVar(value="sprite")
         self.grid_summary_var = tk.StringVar()
         self.status_var = tk.StringVar(
@@ -441,8 +440,6 @@ class PixelArtApp:
             and current_tool in ("Select", "Paste")
         ):
             return self.cancel_selection()
-        if tool_name != "Select":
-            self.last_paint_tool = tool_name
         self.brush_shape_var.set(tool_name)
         self.canvas.focus_set()
         return "break"
@@ -457,12 +454,12 @@ class PixelArtApp:
         self.canvas.delete("selection")
 
         if selection_tool_active:
-            self.brush_shape_var.set(self.last_paint_tool)
+            self.brush_shape_var.set("Select")
 
         self.canvas.focus_set()
         if had_selection or selection_tool_active:
             self.status_var.set(
-                f"Selection cleared  •  {self.last_paint_tool} brush active"
+                "Selection reset  •  Drag to make a new selection"
             )
         else:
             self.status_var.set(
